323d08c2e5d45943587d483bf54622d9aaf189d6
Subham Ghosh <74487802+subhamgcon@users.noreply.github.com>
2023-04-09 15:32:51 +0530

    Add files via upload

---
+++ Travel/script.js
@@ -0,0 +1,23 @@
+var firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  // Handle User Authentication
+  const auth = firebase.auth();
+
+  function signUp(){
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+    const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+    promise.catch(e => alert(e.message));
+
+    alert("Signed Up");
+  }
